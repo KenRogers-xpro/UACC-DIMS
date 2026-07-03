1b7b4d4b39ddf61722021d4e3ca28e0f32777cd1
KenRogers-xpro <kenxpro0@gmail.com>
2026-07-03 17:27:44 +0300

    chore: update code structure and remove redundant changes

--- .next/dev/static/chunks/src_app_layout_jsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1e8jqm7._.css","static/chunks/node_modules_0k407n1._.js","static/chunks/src_app_providers_jsx_1ipigrq._.js"],
-    source: "dynamic"
-});
